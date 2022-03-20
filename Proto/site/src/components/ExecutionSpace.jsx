773201a6bab70731f9af9ee0c801c9ebe1bc7118
William Taylor <williamtaylordev@gmail.com>
2022-03-20 14:06:27 +0000

    Add action buttons

--- Proto/site/src/components/ExecutionSpace.jsx
+++ Proto/site/src/components/ExecutionSpace.jsx
@@ -7,15 +7,29 @@ export default class ExecutionSpace extends react.Component {
       <div id="execution-space">
         <div id="execution-space-header">
           <h2>Execution Space</h2>
-          <p>... and compile/run it here.</p>
+          <p>... and configure, compile, and run it here.</p>
         </div>
         
         <textarea id="execution-space-input"></textarea>
         <div id="execution-space-actions">
-          
+          <button id="run-action" onClick={this.run}>Run</button>
+          <button id="build-action" onClick={this.build}>Build</button>
+          <button id="configure-action" onClick={this.configure}>Configure</button>
         </div>
         <div id="execution-space-output"></div>
       </div>
     );
+  }
+
+  configure() {
+    //
+  }
+
+  build() {
+    //
+  }
+
+  run() {
+    //
   }
 }
