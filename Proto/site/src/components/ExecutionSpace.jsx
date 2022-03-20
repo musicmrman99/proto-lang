601de4cadb59f9dfdec07732b7791f399d236002
William Taylor <williamtaylordev@gmail.com>
2022-03-20 22:31:54 +0000

    Split right pane into Build/Run tabs

--- Proto/site/src/components/ExecutionSpace.jsx
+++ Proto/site/src/components/ExecutionSpace.jsx
@@ -1,7 +1,25 @@
 import react from "react";
 import './ExecutionSpace.css'
 
+import { Tabs, Tab } from "./utils/Tabs";
+
 export default class ExecutionSpace extends react.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // Build I/O
+      buildConfig: {},
+      buildLog: [],
+
+      // Build result
+      ast: null,
+
+      // Run I/O
+      programInput: "",
+      programOutput: ""
+    };
+  }
+
   render() {
     return (
       <div id="execution-space">
@@ -10,13 +28,35 @@ export default class ExecutionSpace extends react.Component {
           <p>... and configure, compile, and run it here.</p>
         </div>
         
-        <textarea id="execution-space-input"></textarea>
-        <div id="execution-space-actions">
-          <button id="run-action" onClick={this.run}>Run</button>
-          <button id="build-action" onClick={this.build}>Build</button>
-          <button id="configure-action" onClick={this.configure}>Configure</button>
-        </div>
-        <div id="execution-space-output"></div>
+        <Tabs swapOn="hover">
+          <Tab tabid="build" name="Build">
+            <div id="execution-space-main">
+              <p>Build Config:</p>
+              <textarea id="execution-space-input" value={this.input} onChange={(e) => this.props.setInput(e.target.value)}></textarea>
+
+              <div id="execution-space-actions">
+                <button id="build-action" onClick={this.build}>Build</button>
+              </div>
+
+              <p>Build Log:</p>
+              <div id="execution-space-output"></div>
+            </div>
+          </Tab>
+
+          <Tab tabid="run" name="Run">
+            <div id="execution-space-main">
+              <p>Program Input:</p>
+              <textarea id="execution-space-input" value={this.input} onChange={(e) => this.props.setInput(e.target.value)}></textarea>
+
+              <div id="execution-space-actions">
+                <button id="run-action" onClick={this.run}>Run</button>
+              </div>
+
+              <p>Program Output:</p>
+              <div id="execution-space-output"></div>
+            </div>
+          </Tab>
+        </Tabs>
       </div>
     );
   }
---
+++ Proto/site/src/components/utils/Tabs.jsx
@@ -0,0 +1,86 @@
+import react from "react";
+import './Tabs.css';
+
+/**
+ * Show a tabbed pane.
+ * 
+ * Assumes its children are `<Tab />` instances, or otherwise accept the same props.
+ */
+export class Tabs extends react.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: null
+    };
+  }
+
+  render() {
+    return (
+      <div className="tabs">
+        {this.getTabs()}
+        {this.getActiveTabContents()}
+      </div>
+    );
+  }
+
+  // Get the list of tab components, if any.
+  getTabs() {
+    if (react.Children.count(this.props.children) === 0) return null;
+    return (
+      <div className="tabs-header">
+        {react.Children.map(this.props.children, (child) => (
+          <button
+            key={child.props.tabid}
+            className={"tab" + child.props.className != null ? " "+child.props.className : ""}
+            id={child.props.id}
+            onClick={() => this.activateTab(child.props.tabid)}
+          >
+            {child.props.name}
+          </button>
+        ))}
+      </div>
+    );
+  }
+
+  // Get the content of the active tab, if any.
+  getActiveTabContents() {
+    // No children
+    if (react.Children.count(this.props.children) === 0) {
+      return null;
+    }
+
+    // Nothing active (default to first child)
+    if (this.state.active == null) {
+      return react.Children.toArray(this.props.children)[0];
+    }
+
+    // Selected child
+    const first = react.Children.toArray(this.props.children).find((tab) => tab.props.tabid === this.state.active);
+    if (first !== undefined) return first;
+
+    // Selected child does not exist (tabs have changed since last selection; reset active tab)
+    this.setState({active: null});
+  }
+
+  // Active the tab with the given tab ID
+  activateTab(tabid) {
+    this.setState({active: tabid});
+  }
+}
+
+/**
+ * A wrapper for its children that requires the following props:
+ * @prop {string} tabID The ID for this tab. Must be unique across all tabs in this group.
+ * @prop {string} name The name for this tab. Will be shown on the tab itself.
+ * 
+ * The id and className props are forwarded to the underlying element.
+ */
+export class Tab extends react.Component {
+  render() {
+    return (
+      <div className="tab-content">
+        {this.props.children}
+      </div>
+    )
+  }
+}
