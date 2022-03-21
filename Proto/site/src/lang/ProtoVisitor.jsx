22acf4b17a45cb12e567e05436092144137b06be
William Taylor <williamtaylordev@gmail.com>
2022-03-21 20:22:09 +0000

    Add pre-decl/link sentence fragments and operators
    Also, merge sentence fragments.

--- Proto/site/src/lang/ProtoVisitor.jsx
+++ Proto/site/src/lang/ProtoVisitor.jsx
@@ -1,10 +1,27 @@
 import ProtoParserVisitor from './build/ProtoParserVisitor';
 
+const TempType = Object.freeze({
+    SENTENCE_FRAGMENT: Symbol("sentence-fragment"),
+    MERGED_SENTENCE_FRAGMENT: Symbol("merged-sentence-fragment"),
+    ASSOCIATION_OPERATOR: Symbol("association-operator"),
+    DECLARATION_OPERATOR: Symbol("declaration-operator"),
+    PLACEHOLDER_OPERATOR: Symbol("placeholder-operator"),
+});
+
 export const Type = Object.freeze({
-    SOFT_TERMINATOR: Symbol("soft-terminator"),
+    // number, string, boolean - use JS equivalents
+
     PARAMETER: Symbol("parameter"),
+
     MAP: Symbol("map"),
-    BLOCK: Symbol("block")
+    BLOCK: Symbol("block"),
+
+    ASSOCIATION: Symbol("association"),
+    DECLARATION: Symbol("declaration"),
+    PLACEHOLDER: Symbol("placeholder"),
+
+    SENTENCE: Symbol("sentence"),
+    SOFT_TERMINATOR: Symbol("soft-terminator"),
 });
 
 // This class defines a complete visitor for a parse tree produced by ProtoParser.
@@ -62,6 +79,47 @@ export default class ProtoVisitor extends ProtoParserVisitor {
         };
     }
 
+    /* Pre-Sentence Parsing Representations
+    -------------------- */
+
+    // Translate sentence_fragment and the various syntactic operators
+    // into their pre-sentence parsing AST representations.
+
+    // Use Type.SENTENCE for now - they'll be joined during sentence parse/link
+    visitSentence_fragment = (ctx) => ({
+        type: TempType.SENTENCE_FRAGMENT,
+        content: ctx.getText()
+        // Type.SENTENCE has a `ref` too (and its `content` is different)
+    });
+    visitDeclaration_operator = () => ({
+        type: TempType.DECLARATION_OPERATOR
+        // Type.DECLARATION has a `template` and `ref` too
+    });
+    visitPlaceholder_operator = () => ({
+        type: TempType.PLACEHOLDER_OPERATOR
+    });
+    visitAssociation_operator = (ctx) => ({
+        type: TempType.ASSOCIATION_OPERATOR,
+
+        relation: {
+            // List all of them, as the symbols might change in future in a way
+            // that string manipulation won't work, eg. `---` becomes `--`, and
+            // `-->` becomes `->`. `<->` and `</>` may also be removed.
+            "---": { leftDir: true, rightDir: true },
+            "<->": { leftDir: true, rightDir: true },
+            "-->": { leftDir: false, rightDir: true },
+            "<--": { leftDir: true, rightDir: false },
+            "-/-": { leftDir: true, rightDir: true },
+            "</>": { leftDir: true, rightDir: true },
+            "-/>": { leftDir: false, rightDir: true },
+            "</-": { leftDir: true, rightDir: false },
+        }[ctx.ASSOCIATION().getText()],
+
+        remove: ctx.ASSOCIATION().getText().includes("/")
+
+        // Type.ASSOCIATION has a `left` and a `right` too
+    });
+
     /* Sentence Parsing Contexts
     -------------------- */
 
@@ -69,13 +127,59 @@ export default class ProtoVisitor extends ProtoParserVisitor {
     visitNewline = () => ({type: Type.SOFT_TERMINATOR});
 
     // Program (entry point) and Compound Literals
-	visitProgram = (ctx) => ({ type: Type.BLOCK, children: this.parseSentenceContext(ctx) });
-    visitBlock_literal = (ctx) => ({ type: Type.BLOCK, children: this.parseSentenceContext(ctx) });
-    visitMap_literal = (ctx) => ({ type: Type.MAP, children: this.parseSentenceContext(ctx) });
+	visitProgram = (ctx) => ({ type: Type.BLOCK, children: this.parseSentenceDeclContext(ctx) });
+    visitBlock_literal = (ctx) => ({ type: Type.BLOCK, children: this.parseSentenceDeclContext(ctx) });
+    visitMap_literal = (ctx) => ({ type: Type.MAP, children: this.parseSentenceExprContext(ctx) });
 
     parseSentenceContext = (ctx) => {
-        return this.visitChildren(ctx)        // Expressions -> their values / representations
-            .filter((child) => child != null) // Remove EOF, `{`, `}`, `[`, `]`, and dropped nodes
+        // Get raw children
+        let children = this.visitChildren(ctx) // Expressions -> their values / representations
+            .filter((child) => child != null); // Remove EOF, `{`, `}`, `[`, `]`, and dropped nodes
+
+        // Merge SENTENCE_FRAGMENT nodes (they'll be split correctly later), while
+        // passing along other node types.
+        const childrenPostMerge = [];
+        let mergedSentenceFragment = [];
+        const pushMergedSentenceFragment = () => {
+            if (mergedSentenceFragment.length > 0) {
+                childrenPostMerge.push({
+                    type: TempType.MERGED_SENTENCE_FRAGMENT,
+                    content: mergedSentenceFragment
+                        .map((sentenceFragment) => sentenceFragment.content)
+                        .join("")
+                });
+                mergedSentenceFragment = [];
+            }
+        };
+        for (const child of children) {
+            if (child.type === TempType.SENTENCE_FRAGMENT) {
+                mergedSentenceFragment.push(child);
+            } else {
+                pushMergedSentenceFragment();
+                childrenPostMerge.push(child);
+            }
+        }
+        pushMergedSentenceFragment();
+        children = childrenPostMerge;
+
+        // Return transformed children
+        return children;
+    }
+
+    parseSentenceDeclContext = (ctx) => {
+        let children = this.parseSentenceContext(ctx);
+
+        //
+
+        return children;
+    }
+
+    parseSentenceExprContext = (ctx) => {
+        const children = this.parseSentenceContext(ctx);
+
+        //
+
+        return children;
     }
 
     /*
