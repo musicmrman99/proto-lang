b2291cfc592a823629d0890332c9ac5968e83dd2
William Taylor <williamtaylordev@gmail.com>
2022-03-21 19:05:51 +0000

    Reorganise into standard sentence context parser

--- Proto/site/src/lang/ProtoVisitor.jsx
+++ Proto/site/src/lang/ProtoVisitor.jsx
@@ -68,28 +68,15 @@ export default class ProtoVisitor extends ProtoParserVisitor {
     // Translate newline -> SOFT_TERMINATOR
     visitNewline = () => ({type: Type.SOFT_TERMINATOR});
 
-    // Program (entry point)
-	visitProgram = (ctx) => ({
-        type: Type.BLOCK,
-        astNodes:
-            this.visitChildren(ctx)           // Expressions -> their values / representations
-            .filter((child) => child != null) // Remove EOF and dropped nodes
-    });
+    // Program (entry point) and Compound Literals
+	visitProgram = (ctx) => ({ type: Type.BLOCK, children: this.parseSentenceContext(ctx) });
+    visitBlock_literal = (ctx) => ({ type: Type.BLOCK, children: this.parseSentenceContext(ctx) });
+    visitMap_literal = (ctx) => ({ type: Type.MAP, children: this.parseSentenceContext(ctx) });
 
-    // Compound literals
-    visitBlock_literal = (ctx) => ({
-        type: Type.BLOCK,
-        astNodes:
-            this.visitChildren(ctx)           // Expressions -> their values / representations
-            .filter((child) => child != null) // Remove `{`, `}`, and dropped nodes
-    });
-
-    visitMap_literal = (ctx) => ({
-        type: Type.MAP,
-        astNodes:
-            this.visitChildren(ctx)           // Expressions -> their values / representations
-            .filter((child) => child != null) // Remove `[`, `]`, and dropped nodes
-    });
+    parseSentenceContext = (ctx) => {
+        return this.visitChildren(ctx)        // Expressions -> their values / representations
+            .filter((child) => child != null) // Remove EOF, `{`, `}`, `[`, `]`, and dropped nodes
+    }
 
     /*
     Remaining:
