deb1d2b47a12c7e59a18f983d0bad4cc20987d01
William Taylor <williamtaylordev@gmail.com>
2023-03-11 13:46:33 +0000

    Use icons instead of tab orientation to reduce size
    This improves the usability and stability of the system, as the tab
    orientation feature had some known UI bugs.

--- site/src/components/ExecutionSpace.jsx
+++ site/src/components/ExecutionSpace.jsx
@@ -13,20 +13,20 @@ export default class ExecutionSpace extends react.Component {
   render() {
     return (
       <div id="execution-space">
-        <Tabs tabLocation="top" tabOrientation="top" swapEvent="onMouseEnter">
-          <Tab tabid="build-config" name="Build Config">
+        <Tabs location="right" iconLocation="right" swapEvent="onMouseEnter">
+          <Tab tabid="build-config" label="Build Config">
             <BuildConfigPanel buildConfig={this.props.buildConfig} onBuildConfigChange={this.props.onBuildConfigChange} />
           </Tab>
 
-          <Tab tabid="build-log" name="Build Log">
+          <Tab tabid="build-log" label="Build Log">
             <BuildLogPanel buildLog={this.props.buildLog} />
           </Tab>
 
-          <Tab tabid="program-input" name="Program Input">
+          <Tab tabid="program-input" label="Program Input">
             <ProgramInputPanel programInput={this.props.programInput} onProgramInputChange={this.props.onProgramInputChange} />
           </Tab>
 
-          <Tab tabid="program-output" name="Program Output">
+          <Tab tabid="program-output" label="Program Output">
             <ProgramOutputPanel programOutput={this.props.programOutput} />
           </Tab>
         </Tabs>
--- site/src/components/utils/Tabs.jsx
+++ site/src/components/utils/Tabs.jsx
@@ -15,73 +15,86 @@ export class Tabs extends react.Component {
   }
 
   render() {
+    const location = this.getLocation();
+
+    const tabsElem = (<react.Fragment key="tabs-header">{this.getTabs(location)}</react.Fragment>);
+    const activeTabContentsElem = (<react.Fragment key="tab-content">{this.getActiveTabContents()}</react.Fragment>);
+
     let content = [];
-    switch (this.props.tabLocation) {
-      case "top": content = [this.getTabs(), this.getActiveTabContents()]; break;
-      case "left": content = [this.getTabs(), this.getActiveTabContents()]; break;
-      case "bottom": content = [this.getActiveTabContents(), this.getTabs()]; break;
-      case "right": content = [this.getActiveTabContents(), this.getTabs()]; break;
-      default: throw new Error(`No such tab location: '${this.props.tabLocation}'`);
+    switch (location) {
+      case "top":
+      case "left":
+        content = [tabsElem, activeTabContentsElem]; break;
+
+      case "bottom":
+      case "right":
+        content = [activeTabContentsElem, tabsElem]; break;
+
+      default:
     }
 
-    const locationClass = {
-      "top": "",
-      "left": " vertical-location",
-      "bottom": "",
-      "right": " vertical-location"
-    }[this.props.tabLocation];
-
     return (
-      <div className={"tabs" + locationClass}>
+      <div className={"tabs" + this.getClassForLocation(location)}>
         {content}
       </div>
     );
   }
 
   // Get the list of tab components, if any.
-  getTabs() {
+  getTabs = (location) => {
     // No children
     if (react.Children.count(this.props.children) === 0) return null;
 
-    const locationClass = {
-      "top": "",
-      "left": " vertical-location",
-      "bottom": "",
-      "right": " vertical-location"
-    }[this.props.tabLocation];
-
-    const orientationClass = {
-      "top": "",
-      "left": " left-orientation",
-      "right": " right-orientation"
-    }[this.props.tabOrientation];
-
     return (
-      <div className={"tabs-header" + locationClass + orientationClass}>
-        {react.Children.map(this.props.children, (child) => {
-          const tabProps = {
-            "key": child.props.tabid,
-            "className": (
-              "tab" +
-              (child.props.tabid === this.state.active ? " active" : "") +
-              (child.props.className != null ? " "+child.props.className : "")
-            ),
-            "id": child.props.id,
-            [this.props.swapEvent]: () => this.activateTab(child.props.tabid)
-          };
-
-          return (
-            <button {...tabProps}>
-              {child.props.name}
-            </button>
-          );
-        })}
+      <div className={"tabs-header" + this.getClassForLocation(location)}>
+        {react.Children.map(this.props.children, this.getTab)}
       </div>
     );
   }
 
+  getTab = (child) => {
+    const iconLocation = this.getIconLocation();
+
+    const labelElem = (<span key="label">{child.props.label}</span>);
+    const iconElem = this.getIconComponentFor(child);
+
+    let content = [];
+    switch (iconLocation) {
+      case "top":
+      case "left":
+        content = [iconElem, labelElem]; break;
+
+      case "bottom":
+      case "right":
+        content = [labelElem, iconElem]; break;
+
+      default:
+    }
+
+    const tabProps = {
+      "key": child.props.tabid,
+      "id": child.props.id,
+      "className": (
+        // Our classes
+        "tab" +
+        (child.props.tabid === this.state.active ? " active" : "") +
+        this.getClassForIconLocation(iconLocation) +
+
+        // Given classes
+        (child.props.className != null ? " "+child.props.className : "")
+      ),
+      [this.props.swapEvent]: () => this.activateTab(child.props.tabid)
+    };
+
+    return (
+      <button {...tabProps}>
+        {content}
+      </button>
+    );
+  }
+
   // Get the content of the active tab, if any.
-  getActiveTabContents() {
+  getActiveTabContents = () => {
     // No children
     if (react.Children.count(this.props.children) === 0) return null;
 
@@ -98,9 +111,50 @@ export class Tabs extends react.Component {
     this.setState({active: null});
   }
 
+  /* Actions
+  -------------------- */
+
   // Active the tab with the given tab ID
   activateTab(tabid) {
     this.setState({active: tabid});
+  }
+
+  /* Utils
+  -------------------- */
+
+  getIconComponentFor = (child) => (
+    child.props.icon != null ?
+      <span key="icon" className="icon material-symbols-outlined">{child.props.icon}</span> :
+      null
+  );
+
+  getLocation = () => (
+    this.props.location != null ?
+      this.props.location :
+      "top"
+  );
+  getIconLocation = () => (
+    this.props.iconLocation != null ?
+      this.props.iconLocation :
+      this.getLocation() // Default to same as location
+  );
+
+  getClassForLocation = (location) => {
+    return {
+      "top": "",
+      "left": " location-vertical",
+      "bottom": "",
+      "right": " location-vertical"
+    }[location];
+  }
+
+  getClassForIconLocation = (iconLocation) => {
+    return {
+      "top": " icon-location-vertical icon-location-end",
+      "left": " icon-location-end",
+      "bottom": " icon-location-vertical",
+      "right": ""
+    }[iconLocation];
   }
 }
 
