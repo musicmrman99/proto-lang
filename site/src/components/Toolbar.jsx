3dce1f40f9d1ec406386570fc3c1471e1bebb004
William Taylor <williamtaylordev@gmail.com>
2023-04-15 17:28:45 +0100

    Fix prop name: class -> className

--- site/src/components/Toolbar.jsx
+++ site/src/components/Toolbar.jsx
@@ -8,16 +8,16 @@ import commands from '../core/Commands';
 export default class Toolbar extends react.Component {
   render() {
     return (
-      <div class="toolbar">
+      <div className="toolbar">
         <h2>Tools</h2>
         <button onClick={() => this.props.onBuild(...commands.build(this.props.protoSource, this.props.buildConfig))}>
-          <span class="material-symbols-outlined">construction</span>
+          <span className="material-symbols-outlined">construction</span>
           <br />
           Build
         </button>
 
         <button onClick={() => this.props.onRun(commands.run(this.props.ast, this.props.programInput))}>
-          <span class="material-symbols-outlined">settings</span>
+          <span className="material-symbols-outlined">settings</span>
           <br />
           Run
         </button>
