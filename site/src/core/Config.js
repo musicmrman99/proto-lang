6eba94735a826860412a88a197e1a11f04d04ed6
William Taylor <williamtaylordev@gmail.com>
2023-05-07 11:32:06 +0100

    Remove escape sequence config (another way is better)

--- site/src/core/Config.js
+++ site/src/core/Config.js
@@ -1,33 +1,8 @@
 export const configSchema = {
   title: "Proto Config",
-  description: "Configuration data for the Proto programming language",
+  description: "Configuration data for the Proto programming language interpreter",
 
-  definitions: {
-    specialEscapeSequenceCategory: {
-      type: "string",
-      enum: [
-        // Value             // Example         | Description
-        // ------------------------------------------------------------------------------------------------------------------------------
-        "keep",              // "\n" -> "\n"    | Keep the entire escape sequence.
-        "translate-literal", // "\n" -> "n"     | Remove the backslash.
-        "translate-special", // "\n" -> newline | Remove the entire escape sequence and replace with the corresponding special character.
-        "drop",              // "\n" -> ""      | Remove the entire escape sequence.
-        "error"              // "\n" -> ERROR   | Throw an error when any escape sequence in this category is found.
-      ]
-    },
-
-    literalEscapeSequenceCategory: {
-      type: "string",
-      enum: [
-        // Value             // Example       | Description
-        // ------------------------------------------------------------------------------------------------------------------------------
-        "keep",              // "\n" -> "\n"  | Keep the entire escape sequence.
-        "translate-literal", // "\n" -> "n"   | Remove the backslash.
-        "drop",              // "\n" -> ""    | Remove the entire escape sequence.
-        "error"              // "\n" -> ERROR | Throw an error when any escape sequence in this category is found.
-      ]
-    }
-  },
+  definitions: {},
 
   type: "object",
   properties: {
@@ -35,55 +10,7 @@ export const configSchema = {
       description: "Settings for Proto itself",
 
       type: "object",
-      properties: {
-        string: {
-          description: "String (\"...\") handling settings",
-
-          type: "object",
-          properties: {
-            escapeSequence: {
-              description: "Escape sequence (\\...) handling settings",
-
-              type: "object",
-              properties: {
-                special: {
-                  description: "The policies to apply for all special escape sequences (ie. those which could translate to a different character)",
-
-                  type: "object",
-                  properties: {
-                    default: {
-                      description: "The default policy to apply for all special escape sequences (ie. those which could translate to a different character)",
-                      "$ref": "#/definitions/specialEscapeSequenceCategory"
-                    }
-                  }
-                },
-
-                literal: {
-                  description: "The policies to apply for all non-special escape sequences (ie. those which translate to the literal character after the backslash)",
-
-                  type: "object",
-                  properties: {
-                    default: {
-                      description: "The default policy to apply for all non-special escape sequences (ie. those which translate to the literal character after the backslash)",
-                      "$ref": "#/definitions/literalEscapeSequenceCategory"
-                    },
-                    openCloseString: {
-                      description: "The policy to apply for escaped \" (double-quote)",
-                      "$ref": "#/definitions/literalEscapeSequenceCategory"
-                    },
-                    openEscapeSequence: {
-                      description: "The policy to apply for escaped \\ (backslash)",
-                      "$ref": "#/definitions/literalEscapeSequenceCategory"
-                    }
-                  }
-                }
-              },
-              additionalProperties: false
-            }
-          },
-          additionalProperties: false
-        }
-      },
+      properties: {},
       additionalProperties: false
     }
   },
@@ -91,18 +18,5 @@ export const configSchema = {
 };
 
 export const configDefault = {
-  core: {
-    string: {
-      escapeSequence: {
-        special: {
-          default: "translate-special"
-        },
-        literal: {
-          default: "drop",
-          openCloseString: "translate-literal",
-          openEscapeSequence: "translate-literal"
-        }
-      }
-    }
-  }
+  core: {}
 };
